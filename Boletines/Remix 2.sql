855651b01e8791a8ec0e9aca861fc04cba2fd22a
Alberto <114813566+AlbertoF17@users.noreply.github.com>
2022-11-30 12:31:53 +0100

    Update Remix 2.sql

--- Boletines/Remix 2.sql
+++ Boletines/Remix 2.sql
@@ -23,4 +23,4 @@ SELECT City FROM suppliers WHERE substring(City,1,1) BETWEEN "A" AND "C" ORDER B
 -- 8. Por cada nombre de compañía de envíos contar el número de distintos productos enviados. Es decir, que si una compañía
 -- de envíos envía una bata, tres bicis y siete jerseys de cuello de pico estampados a un cliente zamorano y luego la misma compañía
 -- de envíos envía una bata a un cliente jienense, esa  compañía de envíos (que Dios la guarde en su alma) habría enviado tres productos distintos.
-SELECT ship.CompanyName, count(orddet.productID) FROM orderdetails AS orddet JOIN orders AS ord ON (orddet.OrderID = ord.OrderID) JOIN shippers AS ship ON (ord.ShipVia = ship.ShipperID) GROUP BY ship.CompanyName;
+SELECT ship.CompanyName, count(distinct(orddet.productID)) FROM orderdetails AS orddet JOIN orders AS ord ON (orddet.OrderID = ord.OrderID) JOIN shippers AS ship ON (ord.ShipVia = ship.ShipperID) GROUP BY ship.CompanyName;
