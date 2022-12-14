7d5c3e8945a59bab3c7a26a892bcccf80e57b412
Alberto <114813566+AlbertoF17@users.noreply.github.com>
2022-12-14 20:49:00 +0100

    Add files via upload

---
+++ Boletín Subselect.sql
@@ -0,0 +1,205 @@
+-- 0. Ciudades que tienen menos clientes (customers) que la ciudad de Buenos Aires.
+SELECT City, count(*) AS "Cantidad de Clientes"
+FROM customers GROUP BY city
+HAVING count(*)<
+(SELECT count(*)
+FROM customers
+WHERE City="Buenos Aires")
+ORDER BY count(*) DESC;
+
+-- 1. Productos cuyo valor de unidades en stock sea superior al valor máximo de unidades en stock de los productos de la categoría 4.
+SELECT *
+FROM products
+WHERE UnitsInStock >
+(SELECT MAX(UnitsInStock)
+FROM products
+WHERE CategoryID=4)
+ORDER BY UnitsInStock ASC;
+
+-- 2. Nombre de los empleados que son jefes.
+SELECT FirstName
+FROM employees
+WHERE ReportsTo IS NULL; -- (WTF se supone q necesita subselect)
+
+-- 3. Productos cuya categoría empiezan por la letra C.
+SELECT prod.ProductName, cat.CategoryName
+FROM products AS prod
+JOIN categories AS cat
+ON (prod.CategoryID = cat.CategoryID)
+WHERE cat.CategoryID IN
+(SELECT CategoryID FROM categories
+WHERE CategoryName LIKE "C%");
+
+-- 4. Pedidos cuyo valor de carga (Freight) está por encima de la media.
+SELECT *
+FROM orders
+WHERE Freight >
+(SELECT AVG(Freight)
+FROM orders)
+ORDER BY Freight ASC;
+
+-- 5. Productos cuyo valor de unidades en stock sea superior al valor máximo de unidades en stock de los productos de la categoría 4.
+SELECT ProductID, ProductName, UnitsInStock
+FROM products
+WHERE UnitsInStock >
+(SELECT MAX(UnitsInStock)
+FROM products
+WHERE CategoryID = 4)
+ORDER BY UnitsInStock ASC;
+
+-- 6. Escribir una consulta para recuperar el nombre de la compañía e ID de proveedor de aquellos proveedores que viven en un país que
+-- tiene mas proveedores que Alemania. 
+ SELECT CompanyName, SupplierID, count(*)
+ FROM Suppliers
+ WHERE Country IN 
+(SELECT Country
+FROM Suppliers
+GROUP BY Country
+HAVING Count(*) >
+(SELECT Count(*)
+FROM Suppliers
+WHERE Country='Germany'))
+GROUP BY CompanyName;
+
+-- 7. Escribir una consulta para imprimir el nombre, apellidos y edad de aquellos empleados que tienen una edad superior a la edad media.
+SELECT FirstName, LastName, year(curdate())- year(BirthDate) AS "Edad" , BirthDate
+FROM employees
+HAVING year(curdate())- year(BirthDate) >
+(SELECT AVG(year(curdate())- year(BirthDate))
+FROM employees);
+
+-- 8. Escribir una consulta para imprimir el nombre, apellidos y edad de aquellos empleados que tienen una edad superior a la edad media
+-- de aquellos empleados con el título de 'Sales Representative'.
+SELECT FirstName, LastName, year(curdate())- year(BirthDate) AS "Edad" , BirthDate
+FROM employees
+HAVING year(curdate())- year(BirthDate) >
+(SELECT AVG(year(curdate())- year(BirthDate))
+FROM employees
+WHERE Title = "Sales Representative");
+
+-- 9. Escribir una consulta para recuperar todos los datos de los empleados que tienen una edad superior a la edad de Margaret Peacock.
+SELECT *
+FROM employees
+WHERE year(curdate())- year(BirthDate) >
+(SELECT year(curdate())- year(BirthDate)
+FROM employees
+WHERE FirstName="Margaret"
+AND LastName="Peacock");
+
+-- 10. Escribir una consulta para recuperar el ID de pedido, ID de cliente y nombre de compañía. Trabaje sin hacer joins entre orders y customers.
+SELECT OrderID, 
+(SELECT CustomerID
+FROM orders AS ord
+WHERE ord.OrderID = orddet.orderID) AS "CustomerID",
+(SELECT prod.SupplierID
+FROM products AS prod
+WHERE orddet.ProductID = prod.ProductID) AS "CompanyName"
+FROM orderdetails AS orddet;
+
+-- 11. Nombre de compañías de clientes que no han hecho pedidos en el 1996. No usar JOIN.
+SELECT CompanyName
+FROM customers
+WHERE CustomerID IN
+(SELECT CustomerID
+FROM orders
+WHERE YEAR(OrderDate)!=1996)
+GROUP BY CompanyName;
+
+-- 12. Nombre de productos suministrados por proveedores de Japón. No usar JOIN.
+SELECT ProductName
+FROM products
+WHERE SupplierID IN
+(SELECT SupplierID
+FROM suppliers
+WHERE Country="Japan");
+
+-- 13. Hacer una subconsulta para obtener los empleados que tienen como jefe a Andrew Fuller.
+SELECT FirstName
+FROM employees
+WHERE ReportsTo =
+(SELECT EmployeeID
+FROM employees
+WHERE FirstName = "Andrew"
+AND LastName="Fuller");
+
+-- 14. Repetir para mostrar los que no tienen como jefe a Andrew.
+SELECT FirstName
+FROM employees
+WHERE ReportsTo !=
+(SELECT EmployeeID
+FROM employees
+WHERE FirstName = "Andrew"
+AND LastName="Fuller");
+
+-- 15. Recuperar los ID de productos con un precio unitario superior al precio unitario medio.
+SELECT ProductID, UnitPrice
+FROM products
+WHERE UnitPrice>
+(SELECT AVG(UnitPrice)
+FROM products)
+ORDER BY UnitPrice ASC;
+
+-- 16. Subconsulta para obtener los nombres de categorias con un número de productos superior al número de  productos que pertenecen a la categoría 'condiments'.
+-- NO FUNCIONA
+SELECT CategoryName
+FROM categories
+GROUP BY CategoryName
+HAVING count(*) >
+(SELECT count(*)
+FROM products
+WHERE CategoryID =
+(SELECT CategoryID
+FROM categories
+WHERE CategoryName = "Condiments"));
+
+-- 17. Subconsulta para obtener los ID de aquellos empleados que tienen un número de pedidos superior a la media de pedidos por empleado.
+SELECT EmployeeID
+FROM orders
+GROUP BY EmployeeID
+HAVING count(*)>
+(SELECT AVG(count(*))
+FROM orders
+GROUP BY EmployeeID);
+
+-- 18. Productos cuyo valor de unidades en stock sea superior al valor máximo de unidades en stock de los productos de la categoría 1 y 3.
+-- SELECT ProductName WHERE ;
+
+-- 19. Escribir una consulta para recuperar todos los datos de los empleados que tienen una edad inferior a la edad de Anne Dodsworth o Janet Leverling.
+
+
+-- 20. Escribir una consulta para recuperar los paises que tiene mas proveedores que la suma de proveedores que tienen Brasil y España.
+
+
+-- 21. Escribir una consulta para recuperar el nombre de la compañía e ID de proveedor de aquellos proveedores que viven en un país que tiene mas
+-- proveedores que la suma de proveedores que tienen Brasil y España.
+
+
+-- 22. Ciudades que tienen más clientes que Madrid.
+
+
+-- 23. Ciudades que tienen más clientes que Madrid y Sevilla o Seville.
+
+
+-- 24. Ciudades que tienen más clientes que Madrid o Sevilla o Seville.
+
+-- 25. Ciudades que tienen más clientes que la suma de clientes de Madrid, Sevilla o Seville y Lisboa.
+
+
+-- 26. Escribir una consulta para imprimir el nombre, apellidos y edad de aquellos empleados que tienen una edad igual o superior a la edad media.
+
+
+-- 27. Escribir una consulta para imprimir el nombre, apellidos y edad de aquellos empleados que tienen una edad igual o superior a la edad media de los
+-- empleados con el cargo Sales Representative.
+
+
+-- 28. Productos cuyo valor de unidades en stock sea superior al valor mínimo de unidades en stock de los productos de la categoría 4 o superior al valor
+-- mínimo de unidades en stock de los productos de la categoría 6.
+
+
+-- 29. Productos cuya categoría empieza por la letra C o D.
+
+
+-- 30. Ciudades que tienen menos clientes (customers) que la ciudad de Buenos Aires y Munich.
+
+
+-- 31. Empleados que son más jóvenes que Margaret, Laura y Michael.
